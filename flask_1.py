716f5c8fc86e2e7a6ba09efb25774ddf76b3c121
sdfxisme <sdfx@yandex.ru>
2020-04-28 15:53:52 +0300

    ок

--- flask_1.py
+++ flask_1.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
-import sqlite3 as lite
-import sys
+from sqlalchemy import Column, Integer, String, Float, create_engine, ForeignKey, Table
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 import requests
 app = Flask(__name__)
 
@@ -48,22 +50,42 @@ def city_form():
             if sal_from != None: salary_from.append(sal_from)
             if sal_to != None: salary_to.append(sal_to)
 
-    mid_sal_from = int(sum(salary_from) / len(salary_from))
-    mid_sal_to = int(sum(salary_to) / len(salary_to))
+    salary_from = int(sum(salary_from) / len(salary_from))
+    salary_to = int(sum(salary_to) / len(salary_to))
 
 
     data = {'city': city,
             'prof': prof,
-            'salary_from': mid_sal_from,
-            'salary_to': mid_sal_to}
+            'salary_from': salary_from,
+            'salary_to': salary_to}
 
-    connect = None
-    connect = lite.connect('test.db')
-    with connect:
-        cur = connect.cursor()
-        #cur.execute("CREATE TABLE hh(city TEXT, prof TEXT, sallary_from INT, sallary_to INT)")
-        cur.execute("INSERT INTO hh VALUES(?,?,?,?)", (data['city'], data['prof'], data['salary_from'], data['salary_to']))
-        #connect.close()
+    engine = create_engine('sqlite:///orm1.sqlite', echo=False)
+    Base = declarative_base()
+
+    class HH_request(Base):
+        __tablename__ = 'HH_request_new'
+        id = Column(Integer, primary_key=True)
+        city = Column(String)
+        prof = Column(String)
+        salary_from = Column(Integer)
+        salary_to = Column(Integer)
+
+        def __init__(self, city, prof, salary_from, salary_to):
+            self.city = city
+            self.prof = prof
+            self.salary_from = salary_from
+            self.salary_to = salary_to
+
+        def __str__(self):
+            return f'{self.city},{self.prof},{self.salary_from},{self.salary_to}'
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    HH_request_1 = HH_request(city, prof, salary_from, salary_to)
+    session.add(HH_request_1)
+    session.commit()
+
     return render_template('city_form.html', data=data)
 
 @app.route('/hands_to_db')
@@ -72,24 +94,45 @@ def hands_to_db():
 
 @app.route('/hands_to_db_rec', methods=['POST'])
 def hands_to_db_rec():
-    city = request.form['city']
-    prof = request.form['prof']
-    salary_from = request.form['salary_from']
-    salary_to = request.form['salary_to']
-    data = {'city': city,
-            'prof': prof,
-            'salary_from': salary_from,
-            'salary_to': salary_to}
+    city_hotelka = request.form['City']
+    prof_hotelka = request.form['Prof']
+    salary_from_hotelka = request.form['Salary_from']
+    salary_to_hotelka = request.form['Salary_to']
 
-    connect = None
-    connect = lite.connect('test.db')
-    with connect:
-        cur = connect.cursor()
-        #cur.execute("CREATE TABLE hhotelka(city TEXT, prof TEXT, sallary_from INT, sallary_to INT)")
-        cur.execute("INSERT INTO hhotelka VALUES(?,?,?,?)", (city, prof, salary_from, salary_to))
-        # connect.close()
-    return render_template('hands_to_db_rec.html', data = data)
+    data = {'city': city_hotelka,
+            'prof': prof_hotelka,
+            'salary_from': salary_from_hotelka,
+            'salary_to': salary_to_hotelka}
 
+    engine = create_engine('sqlite:///orm1.sqlite', echo=False)
+    Base = declarative_base()
+
+    class HH_hotelka(Base):
+        __tablename__ = 'HH_hotelka'
+        id = Column(Integer, primary_key=True)
+        city_hotelka = Column(String)
+        prof_hotelka = Column(String)
+        salary_from_hotelka = Column(Integer)
+        salary_to_hotelka = Column(Integer)
+
+        def __init__(self, city_hotelka, prof_hotelka, salary_from_hotelka, salary_to_hotelka):
+            self.city_hotelka = city_hotelka
+            self.prof_hotelka = prof_hotelka
+            self.salary_from_hotelka = salary_from_hotelka
+            self.salary_to_hotelka = salary_to_hotelka
+
+        def __str__(self):
+            return f'{self.city_hotelka},{self.prof_hotelka},{self.salary_from_hotelka},{self.salary_to_hotelka}'
+
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    HH_hotelka_1 = HH_hotelka(city_hotelka, prof_hotelka, salary_from_hotelka, salary_to_hotelka)
+    session.add(HH_hotelka_1)
+    session.commit()
+
+    return render_template('hands_to_db_rec.html', data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
